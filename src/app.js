2421afa1bd6da344a99495a60cfb161b19f4c42f
Mehmet Tursucu <tursucumehmet@gmail.com>
2022-01-02 17:47:14 +0300

    Assigment 3 - All criteria met.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-import "./styles.css";
 import axios from "axios";
 
 axios
@@ -6,19 +5,22 @@ axios
   .then((response) => {
     // Firstly, log response to the console,
     // inspect the response and see that it has data field
-
+    console.log(response.data)
     // Assign data field of the response to
     // products variable below by destructuring
     // You can use alias
-    const products = null;
+    const products = response.data;
 
     // Print names of all product to the console
     // by calling foreach  method (use arrow function)
+    products.forEach(data => console.log(data.name));
 
     // Get all products that contain "Şal" in their name (use filter method)
     // map filtered products to new object having only image and name field
     // assign mapped items to mappedProducts variable
-    const mappedProducts = null;
+    const mappedProducts = products
+      .filter(({ name, description }) => name.toLowerCase().match(/şal/g) || description.toLowerCase().match(/şal/g))
+      .map(({ name, description, image }) => ({ name, description, image }));
 
     // Display the images and names of mappedProducts
     // You need to add them to the DOM
@@ -26,4 +28,17 @@ axios
     // You need to use flexbox
     // Position of image and text is up to you
     // You can use any style you wish
+    const productElement = document.querySelector(".products");
+
+    mappedProducts.forEach(({ name, description, image }) => {
+      productElement.innerHTML += `
+      <div class="product-area">
+        <div class="product">
+          <img width="310" height="420" src="${image}" alt="${name}" />
+          <div class="name">
+            <span>${description}</span>
+          </div>
+      </div>
+      `
+    });
   });
